00f6df8dd73ce7476efb719febf4a07a2ea85519
Frederik Elwert <frederik.elwert@rub.de>
2025-02-03 21:25:27 +0100

    Activate IIIF support

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,6 +21,7 @@ COLLECTION_ITEM_META = [
     {"label": "Source", "value": "source", "type": "link"},
 ]
 COLLECTION_CATEGORY = "Collection"
+COLLECTION_USE_IIIF = True
 HOME_CATEGORY = "Blog"
 HOME_MAX_ENTRIES = 3
 HOME_HERO_ITEM = "obj2"
